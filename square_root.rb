a9e7b161a85cdc444c6a67a2230638f20b52862d
Andressa Machado <andressa.pamachado@gmail.com>
2022-07-14 12:29:56 -0400

    Calculates sqrt of a number using Binary Search

---
+++ square_root.rb
@@ -0,0 +1,27 @@
+class SquareRoot
+  def initialize(num)
+    @number = num
+    puts sqrt
+  end
+
+  def sqrt
+    low = 1
+    high = @number
+    answer = 0
+
+    while low <= high
+      middle = (low + high)/2
+
+      if middle <= @number/middle 
+        answer = middle
+        low = middle + 1
+      elsif 
+        high = middle - 1
+      end 
+    end
+
+    answer
+  end
+end
+
+subject = SquareRoot.new(8)
