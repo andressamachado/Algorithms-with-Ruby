c791e218124e42f1304bb54ccd61220834d61dad
Andressa Machado <andressa.pamachado@gmail.com>
2022-07-19 13:25:34 -0400

    O(logn) - logarithmic time V2

---
+++ binary_search_v2.rb
@@ -0,0 +1,35 @@
+class BinarySearchV2
+
+  def initialize(item)
+    @list = Array.new(11) {|i| i} 
+    @high = @list.length #highest number to check represented by the index
+    @low = 0 #lowest number to check represented by the index
+    @item = item
+    @moves = 0
+
+    binary_search_v2
+
+    puts "it took #{@moves} iterations to find the number"
+  end 
+
+  def binary_search_v2
+
+    while @low < @high #while we have more than one number to check
+      middle = @low + (@high - @low)/2
+      guess = @list[middle]
+
+      @moves += 1
+
+      return middle if guess == @item
+
+      if guess > @item then @high = middle - 1 end
+      if guess <  @item then @low = middle + 1 end
+    end 
+
+    if @left != @list.length && @list[@left] == target then return @left end
+
+    return -1;
+  end
+end 
+
+subject = BinarySearchV2.new(6)
