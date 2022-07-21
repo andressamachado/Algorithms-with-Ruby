83a5a3c532bc458ef626b319d4272942f23b6014
Andressa Machado <andressa.pamachado@gmail.com>
2022-07-21 15:18:18 -0400

    O(logn) - when requires accessing the current index and its immediate right neighbor's index.

--- binary_search_v2.rb
+++ binary_search_v2.rb
@@ -7,16 +7,13 @@ class BinarySearchV2
     @moves = 0
 
     binary_search_v2
-
     puts "it took #{@moves} iterations to find the number"
   end 
 
   def binary_search_v2
-
     while @low < @high #while we have more than one number to check
       middle = @low + (@high - @low)/2
       guess = @list[middle]
-
       @moves += 1
 
       return middle if guess == @item
@@ -25,7 +22,7 @@ class BinarySearchV2
       if guess <  @item then @low = middle + 1 end
     end 
 
-    if @left != @list.length && @list[@left] == target then return @left end
+    if @low != @list.length && @list[@low] == @item then return @low end
 
     return -1;
   end
